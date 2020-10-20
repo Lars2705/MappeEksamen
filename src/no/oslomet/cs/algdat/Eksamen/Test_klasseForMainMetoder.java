cc8eeb6efa46afb32766991c503ba2cfc01ff55b
lars <abbeilarsh123@gmail.com>
2020-10-20 17:43:36 +0200

    oppgave 0) Added instans av EksamenSBinTree som ble fulgte med i oppgave 0. Førte dette inn i en egen main klasse. kjørte koden og fikk riktig utskrift av koden uten noen unntak. Kode 0 er nå løst

--- src/no/oslomet/cs/algdat/Eksamen/Test_klasseForMainMetoder.java
+++ src/no/oslomet/cs/algdat/Eksamen/Test_klasseForMainMetoder.java
@@ -1,8 +1,13 @@
 package no.oslomet.cs.algdat.Eksamen;
 
+import java.util.Comparator;
+
 public class Test_klasseForMainMetoder {
 
     public static void main(String args[]){
+
+        EksamenSBinTre<String> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
+        System.out.println(tre.antall()); // Utskrift: 0
 
 
     }
