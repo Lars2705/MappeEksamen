e7895562655990df5f28763333d9d590738b0bb2
L.Robertbuell <56067726+Lars2705@users.noreply.github.com>
2020-10-26 12:34:52 +0100

    oppgave 6) Implementerer main metode testen for oppgave 6 for å se at det ble skrevet ur riktige vedier. FIkk verdi 2 og 6 printet og og feilmelding på den siste system.out.println - en. Dette er feil fordi verdiene jeg skal få ut i følge oppgaveteskten er 3 og 5. Har spurt medelever og de sa selv at de fikk ut 2 og 6 når de kjørte testen i main metoden. Dette kan tyde på at det er noe feil i oppgaveteksten når det kommer til den riktige verdien som skal prints ut. Må videre spørre om dette på lab timer for å få et ordentlig svar. Men får uansett riktig på oppgave 6 når jeg kjører den igjennom testen til andre

--- src/no/oslomet/cs/algdat/Eksamen/Test_klasseForMainMetoder.java
+++ src/no/oslomet/cs/algdat/Eksamen/Test_klasseForMainMetoder.java
@@ -55,6 +55,22 @@ public class Test_klasseForMainMetoder {
 
  */
 
+        // tester for oppgave 6
+
+        int[] a = {4,7,2,9,4,10,8,7,4,6,1};
+        EksamenSBinTre<Integer> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
+        for (int verdi : a) tre.leggInn(verdi);
+
+        System.out.println(tre.fjernAlle(4)); // 3
+        tre.fjernAlle(7); tre.fjern(8);
+        System.out.println(tre.antall()); // 5
+       // System.out.println(tre + " " + tre.toString());
+
+        // [1, 2, 6, 9, 10] [10, 9, 6, 2, 1]
+        // OBS: Hvis du ikke har gjort oppgave 4 kan du her bruke toString()
+
+
+
 
 
     }
